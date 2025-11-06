ffe6b094e58541f01c7b20995b5fc1d5332e9c9a
mgnzalezc <mariagnzalezc@yahoo.co.uk>
2025-11-06 14:39:33 +0100

    NOV 06

--- Tema4/primitiva.php
+++ Tema4/primitiva.php
@@ -82,6 +82,46 @@
     <p>7 pierdo, punto gano</p>
     <p>do while hasta que gane</p>
 
+    <h3>Dados</h3>
+    <form action="" method="POST">
+        <input type="submit" value="JUGAR"><br>
+    </form>
+
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $turno = 0;
+            $seguir = true;
+            $punto = 0;
+            
+            while($seguir){
+                echo "Turno $turno";
+                $dado1 = rand(1,6);
+                $dado2 = rand(1,6);
+                echo "Dado 1: $dado1  ||  Dado 2: $dado2";
+                $suma = $dado1 + $dado2;
+                if($turno == 0){ //turno 1
+                    if($suma == 11 || $suma == 7){
+                        echo "Has ganado!!!";
+                        $seguir = false;
+                    }
+                    elseif($suma == 2 || $suma == 12){
+                        echo "Has perdido!!!";
+                        $seguir = false;
+                    } else{
+                        $punto = $sumaDados;
+                    }
+
+                } else{ //resto de turnos
+                    //TERMINAR
+                    
+                }
+                
+                $turno++;
+            }
+
+        }
+
+    ?>
 
 </body>
 </html>
